0020045f1dfa0c4b4d69fc3c26b6de707beac0c7
Diana Vargas <dianitavaror@gmail.com>
2020-04-30 22:07:13 -0500

    Variable

--- js/dia2.js
+++ js/dia2.js
@@ -1,18 +1,74 @@
+function patito(minimo, maximo)
+{
+	var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo ); 
+	return numero;
+}
+
+
 var piedra = 0;
 var papel = 1;
 var tijera = 2;
 
+var opciones = ["Piedra", "Papel", "Tijera"];
+
 var opcionUsuario;
-var opcionMaquina;
+var opcionMaquina = patito(0,2);
 
-opcionUsuario = prompt("Que eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
 
-if (opcionUsuario == piedra) {
-	alert("Elegiste Papel!");
+opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+
+alert("Elegiste" + opciones[opcionUsuario]);
+alert("Javascript eligio " + opciones[opcionMaquina]);
+
+if (opcionUsuario == piedra)
+{
+
+	if (opcionMaquina == piedra) 
+	{
+		alert("Empate");
+	}
+	else if (opcionMaquina == papel)
+	{
+		alert("Perdiste :(");
+	}
+	else if (opcionMaquina == tijera)
+	{
+		alert("Ganaste");
+	}
 }
-else if (opcionUsuario == papel) {
-	alert("Elegiste papel!");
+else if (opcionUsuario == papel)
+{
+	
+	if (opcionMaquina == piedra) 
+	{
+		alert("Ganaste");
+	}
+	else if (opcionMaquina == papel)
+	{
+		alert("Empate");
+	}
+	else if (opcionMaquina == tijera)
+	{
+		alert("Perdiste :(");
+	}
 }
-else if (opcionUsuario == tijera) {
-	alert("Elegiste tijera!");
+else if (opcionUsuario == tijera)
+{
+	
+	if (opcionMaquina == piedra) 
+	{
+		alert("Perdiste :(");
+	}
+	else if (opcionMaquina == papel)
+	{
+		alert("Ganaste");
+	}
+	else if (opcionMaquina == tijera)
+	{
+		alert("Ganaste");
+	}
+}
+else
+{
+	alert("sea serio");
 }
